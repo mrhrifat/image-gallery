580c3ac78e76aea5cbd221b10b6505f9d3b5f7c2
mrhrifat <mrhrifat383759@gmail.com>
2023-11-01 15:12:22 +0600

    build: add image & image card

---
+++ src/components/ImageCard.tsx
@@ -0,0 +1,65 @@
+/**
+ * Title: ImageCard
+ * Description:
+ * Filename: ImageCard.tsx
+ * Path: /src/components/ImageCard.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Created Date: Nov 01, 2023
+ * Last Updated Date: Nov 01, 2023
+ *
+ */
+
+import { FC } from "react";
+import { ImageCardType } from "../utils/type";
+
+const ImageCard: FC<ImageCardType> = ({
+  id,
+  item,
+  dropItem,
+  dragItem,
+  isAvailableSelected,
+  handleSelected,
+}) => {
+  return (
+    <div
+      key={item.url}
+      onDragOver={(event) => event.preventDefault()}
+      onDrop={(event) => dropItem(event, item)}
+      onClick={() => handleSelected(item)}
+      className={`image-card ${id === 0 ? "featured" : "normal"} ${
+        isAvailableSelected(item) ? "selected" : ""
+      } hover:onhover group`}
+    >
+      <div
+        className="relative"
+        draggable={true}
+        onDragStart={() => dragItem(item)}
+      >
+        <img
+          src={item.url}
+          alt={item.name}
+          className={`rounded-md ${
+            id === 0 ? "md:w-[362px] h-[364px]" : "md:w-[175px] h-[173px]"
+          }`}
+        />
+
+        <div
+          className={`absolute top-2 left-2 ${
+            isAvailableSelected(item) ? "block" : "hidden group-hover:block"
+          }`}
+        >
+          <input
+            id="selectedImage"
+            name="selectedImage"
+            type="checkbox"
+            checked={isAvailableSelected(item)}
+            onChange={(event) => event.preventDefault()}
+            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageCard;
---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Title: index
+ * Description:
+ * Filename: index.ts
+ * Path: /src/components/index.ts
+ * Author: Mrh Rifat (Programmer)
+ * Created Date: Nov 01, 2023
+ * Last Updated Date: Nov 01, 2023
+ *
+ */
+// Import and Export
+import AddImage from "./AddImage";
+import ImageCard from "./ImageCard";
+export { AddImage, ImageCard };
